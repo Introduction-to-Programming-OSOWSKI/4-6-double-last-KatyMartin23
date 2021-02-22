0cf13d80067decb908ad30a5d92c289494957123
KatyMartin23 <832006@mystma.org>
2021-02-22 16:08:54 +0000

    doubleLast 2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def doubleLast(j, w):
-
-    j.append(w)
+def doubleLast(j):
+    j.append(len(j))
     return j
-print(doubleLast(["cat", "dog", "pig", "bear"], "bear" ))
+print(doubleLast([1, 2, 3, 4 ,5] ))
